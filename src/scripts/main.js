912561bf152b8794e8adbb10a1383dbabc5bafed
Bohdan <takhaiev@icloud.com>
2025-06-12 07:54:58 +0300

    complite tasc

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,17 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
+const wall = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+const wallRect = wall.getBoundingClientRect();
+const initialSpiderX = spider.offsetLeft + spider.offsetWidth / 2;
+const initialSpiderY = spider.offsetTop + spider.offsetHeight / 2;
+
+wall.addEventListener('click', (e) => {
+  const xInWall = e.clientX - wallRect.left;
+  const yInWall = e.clientY - wallRect.top;
+
+  const translateX = xInWall - initialSpiderX;
+  const translateY = yInWall - initialSpiderY;
+
+  spider.style.transform = `translate(${translateX}px, ${translateY}px)`;
 });
